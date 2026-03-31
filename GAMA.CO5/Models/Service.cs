b7419f0c228c0da8cd78b21c5c4557e88152d19f
ShahadAlharthi <shahadalharthi71@gmail.com>
2026-03-31 13:56:19 +0300

    اضافة تواصل ومنتجات

--- GAMA.CO5/Models/Service.cs
+++ GAMA.CO5/Models/Service.cs
@@ -1,24 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace GAMA.CO5.Models
 {
-    public class ServicesViewModel
-    {
-        public string PageTitle { get; set; } = "خدماتنا التقنية";
-        public List<Service> Services { get; set; } = new();
-    }
-
     public class Service
     {
-        public string Id { get; set; } = "";
+        public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; } = "";
+
+        [Required]
         public string Description { get; set; } = "";
+
         public string Icon { get; set; } = "";
 
         public string ImageUrl { get; set; } = "";
+
         public string BadgeText { get; set; } = "";
+
         public string BadgeColor { get; set; } = "";
 
-        public List<string> Features { get; set; } = new();
+        public string Features { get; set; } = "";
     }
 }
