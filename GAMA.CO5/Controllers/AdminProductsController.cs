9ef42e09d91cff55e2a58def2efe69a1fb2922cf
ShahadAlharthi <shahadalharthi71@gmail.com>
2026-04-02 11:07:37 +0300

    Admin products

---
+++ GAMA.CO5/Controllers/AdminProductsController.cs
@@ -0,0 +1,104 @@
+﻿using GAMA.CO5.Data;
+using GAMA.CO5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GAMA_ASP_MVC_CLEAN.Controllers
+{
+    public class AdminProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Product());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            _context.Products.Add(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var product = _context.Products.FirstOrDefault(p => p.Id == model.Id);
+
+            if (product == null)
+                return NotFound();
+
+            product.Name = model.Name;
+            product.Description = model.Description;
+            product.ImageUrl = model.ImageUrl;
+            product.Category = model.Category;
+            product.IsFeatured = model.IsFeatured;
+            product.BadgeText = model.BadgeText;
+            product.Features = model.Features;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
